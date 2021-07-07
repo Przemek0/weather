6d5338cba2e790a4cf6ae10449374c4762c3a61e
Przemysław Piwoński <przemyslaw.piwonski@gmail.com>
2021-07-07 08:32:06 +0200

    Repositorie for entities. V1 of db migration.

---
+++ src/main/java/pl/piwonski/weather/domain/city/CityRepository.java
@@ -0,0 +1,7 @@
+package pl.piwonski.weather.domain.city;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pl.piwonski.weather.model.City;
+
+public interface CityRepository extends JpaRepository<City, Long> {
+}
