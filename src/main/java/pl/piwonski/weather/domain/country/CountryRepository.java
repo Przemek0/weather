0865af95bf6856adcf39af24e93a65c15f151782
Przemysław Piwoński <przemyslaw.piwonski@gmail.com>
2021-07-07 08:32:06 +0200

    Repositorie for entities. V1 of db migration.

---
+++ src/main/java/pl/piwonski/weather/domain/country/CountryRepository.java
@@ -0,0 +1,7 @@
+package pl.piwonski.weather.domain.country;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pl.piwonski.weather.model.Country;
+
+public interface CountryRepository extends JpaRepository<Country, Integer> {
+}
