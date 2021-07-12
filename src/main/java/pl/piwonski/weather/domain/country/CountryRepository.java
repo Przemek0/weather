6fa7876946920b68996d1ec95e30112910b9d5f5
Przemysław Piwoński <przemyslaw.piwonski@gmail.com>
2021-07-12 17:42:06 +0200

    CountryController with getCountries, createCountry and updateCountry methods.

--- src/main/java/pl/piwonski/weather/domain/country/CountryRepository.java
+++ src/main/java/pl/piwonski/weather/domain/country/CountryRepository.java
@@ -4,4 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import pl.piwonski.weather.model.Country;
 
 public interface CountryRepository extends JpaRepository<Country, Integer> {
+    boolean existsByName(String name);
+
+    boolean existsByCode(String code);
 }
